99d1447ef902bc51aef81f07e8a86539de628a2d
Adam Katz <adamhotep@users.noreply.github.com>
2016-03-07 12:55:20 -0800

    Create Confluence - Hide sidebar.user.js
    Collapse the sidebar upon page load

---
+++ Confluence - Hide sidebar.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        Confluence - Hide sidebar
+// @namespace   https://github.com/adamhotep/userscripts
+// @description Collapse the sidebar upon page load
+// @include     https://confluence.*
+// @include     http://confluence.*
+// @version     1
+// @grant       none
+// @license     GPL
+// ==/UserScript==
+
+// more info at https://stackoverflow.com/a/35853814/519360
+if (AJS) {
+  AJS.toInit(function(){
+    if (AJS.$("div.ia-fixed-sidebar").width() > 55){
+      AJS.Confluence.Sidebar.toggle();
+    }
+  });
+}
