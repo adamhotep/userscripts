2a2b613224b26904c7096930eefad8666815d3aa
Adam Katz <adamhotep@users.noreply.github.com>
2017-03-15 19:14:16 -0700

    your own messages are now subtly dk cyan in color

--- Cisco Spark - Compact UI.user.js
+++ Cisco Spark - Compact UI.user.js
@@ -2,7 +2,7 @@
 // @name         Cisco Spark - Compact UI
 // @namespace    https://github.com/adamhotep/userscripts
 // @downloadURL  https://github.com/adamhotep/userscripts/raw/master/Cisco%20Spark%20-%20Compact%20UI.user.js
-// @version      0.3
+// @version      0.4.20170315
 // @description  A compact user interface for Cisco Spark
 // @author       Adam Katz
 // @match        https://web.ciscospark.com/rooms/*/chat
@@ -21,14 +21,18 @@
   css.appendChild(document.createTextNode(/* syn=css */ `
 
 /* activities */
-#activities :not([class*="icon"]):not([class*="avatar"])	{ font-size:inherit!important; }
+#activities :not([class*="icon"]):not([class*="avatar"]) { font-size:inherit!important; }
 .activity-text p			{ margin:0; }
 #activities .activity-item		{ margin-top:0!important; padding-top:1ex; }
 #activities .activity-item:nth-child(2n):not(.system-message):not(:hover) { background:hsla(240,5%,96%,.3); }
 body.roskilde #activities .activity-item.system-message { padding-top:0!important; }
 #activities .activity-item .content .activity-text, #activities .activity-item .content p.meta { line-height: 1.2em; }
+/* "me" */
+#activities .activity-item.me .display-name	{ color:#548da2!important; }
+#activities .activity-item.me .activity-text	{ color:#046172!important; }
 
 /* conversation list */
+#conversation-list .avatar.badge:before { left:-1.2em; }
 #conversation-list .roomListItem	{ padding:0.15em 0; }
 #conversation-list .roomListItem:nth-child(2n+1):not(:hover):not(.isActive) { background-color:#fbfbfb; }
 #conversation-list div:not(.avatar)	{ font-size:80%; }
