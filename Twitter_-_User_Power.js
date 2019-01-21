1b8b69a3d64676aa5c20e7c105d96cc0b3ab7ff6
Adam Katz <github@khopis.com>
2019-01-21 17:25:34 -0500

    Twitter power: added 'influence' and abstracted code, bumped version to 1.0.0

--- Twitter_-_User_Power.js
+++ Twitter_-_User_Power.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name        Twitter - Stats over time, tweet power
+// @name        Twitter - User "power" and other stats
 // @author      Adam Katz
 // @namespace   https://github.com/adamhotep/userscripts
 // @description User pages: Stats over time, power (tweets/follower)
 // @include     https://twitter.com/*
-// @version     0.2
-// @installURL https://github.com/adamhotep/userscripts/raw/master/Twitter_-_User_tweets_per_day.user.js
+// @version     1.0.0.20181215
+// @installURL  https://github.com/adamhotep/userscripts/raw/master/Twitter_-_User_tweets_per_day.user.js
 // @downloadURL https://github.com/adamhotep/userscripts/raw/master/Twitter_-_User_tweets_per_day.user.js
 // @grant       GM_addStyle
 // ==/UserScript==
@@ -30,6 +30,23 @@ if (typeof GM_addStyle == 'undefined') {
 
 function round(num, decimals=0) {
   return Math.round(num * 10**decimals) / 10**decimals;
+}
+
+function round_units(num) {
+  if      (num < 1)     { num = round(num, 3); }
+  else if (num < 10)    { num = round(num, 2); }
+  else if (num < 100)   { num = round(num, 1); }
+  else if (num < 1000)  { num = round(num); }
+  else if (num > 10**11){ num = round(num/10**9)    + "B"; }
+  else if (num > 10**10){ num = round(num/10**9, 1) + "B"; }
+  else if (num > 10**9) { num = round(num/10**9, 2) + "B"; }
+  else if (num > 10**8) { num = round(num/10**6)    + "M"; }
+  else if (num > 10**7) { num = round(num/10**6, 1) + "M"; }
+  else if (num > 10**6) { num = round(num/10**6, 2) + "M"; }
+  else if (num > 10**5) { num = round(num/1000)     + "K"; }
+  else if (num > 10**4) { num = round(num/1000, 1)  + "K"; }
+  else if (num > 10**3) { num = round(num/1000, 2)  + "K"; }
+  return num;
 }
 
 function rate(count, age) {
@@ -65,6 +82,41 @@ function parse_stat(title, age) {
   return count;
 }
 
+function make_stat(name, proper_name, desc, value) {
+  var full_value = value;
+  value = round_units(value);
+  //console.log(`${name} = ${value} (from ${full_value})`);
+  var node = document.createElement("li");
+
+  node.className = "ProfileNav-item ProfileNav-item--" + name;
+      var link = document.createElement("a");
+      link.appendChild(document.createTextNode(proper_name));
+      link.className = "ProfileNav-stat ProfileNav-stat--link gm "
+        + "u-borderUserColor u-textCenter js-tooltip js-nav u-textUserColor";
+      link.title = round(full_value, 3) + " " + desc;
+      link.setAttribute("data-nav", name);
+      var is_compact = false;
+      if (full_value == value) { is_compact = true; }
+      link.innerHTML = `
+        <span class="ProfileNav-label" aria-hidden="true">${proper_name}</span>
+        <span class="u-hiddenVisually">${proper_name}</span>
+        <span class="ProfileNav-value" data-count="${value}"
+          data-is-compact="${is_compact}">
+          ${value}
+          <p class="rate">${rate(full_value, age)}</p>
+        </span>`;
+      node.appendChild(link);
+
+      var after = stats[stats.length-1].parentNode.parentNode;
+      if (after.nextElementSibling) {
+        after.parentNode.insertBefore(node,
+          after.nextElementSibling);
+      } else {
+        after.parentNode.appendChild(node);
+      }
+
+}
+
 var join = document.querySelector(".ProfileHeaderCard-joinDateText");
 if (join) {
   var then;
@@ -87,57 +139,21 @@ if (join) {
     var lists = parse_stat("lists", age);
     var moments = parse_stat("moments", age);
 
-    // power is fans per tweet (with zero tweets, power = fans).
+
+    // influence is the ratio of followers to following
+    var influence = fans;
+    if (friends > 1) { influence /= friends; }
+    if (influence) {
+      make_stat("influence", "Influence", "Followers/Following", influence);
+    }
+
+    // power is log-linear fans to tweets:
+    //   log(fans) * fans / max(tweets, 1)
     var power = fans;
-    if (tweets > 0) { power /= tweets; }
-    var power_value = power;
-    if      (power < 1)     { power = round(power, 3); }
-    else if (power < 10)    { power = round(power, 2); }
-    else if (power < 100)   { power = round(power, 1); }
-    else if (power < 1000)  { power = round(power); }
-    else if (power > 10**11){ power = round(power/10**9)    + "B"; }
-    else if (power > 10**10){ power = round(power/10**9, 1) + "B"; }
-    else if (power > 10**9) { power = round(power/10**9, 2) + "B"; }
-    else if (power > 10**8) { power = round(power/10**6)    + "M"; }
-    else if (power > 10**7) { power = round(power/10**6, 1) + "M"; }
-    else if (power > 10**6) { power = round(power/10**6, 2) + "M"; }
-    else if (power > 10**5) { power = round(power/1000)     + "K"; }
-    else if (power > 10**4) { power = round(power/1000, 1)  + "K"; }
-    else if (power > 10**3) { power = round(power/1000, 2)  + "K"; }
-
+    if (tweets > 1) { power /= tweets; }
+    power = Math.log(tweets) * power;
     if (power) {
-      //console.log("power = " + power);
-      var power_node = document.createElement("li");
-      power_node.className = "ProfileNav-item ProfileNav-item--power";
-      var power_link = document.createElement("a");
-      power_link.appendChild(document.createTextNode("Power"));
-      power_link.className = "ProfileNav-stat ProfileNav-stat--power "
-        + "u-borderUserColor u-textCenter js-tooltip js-nav u-textUserColor";
-      power_link.title = round(power_value, 3) + " Followers/Tweet";
-      power_link.setAttribute("data-nav", "power");
-      var is_compact = false;
-      if (power_value == power) { is_compact = true; }
-      power_link.innerHTML = `
-        <span class="ProfileNav-label" aria-hidden="true">Power</span>
-        <span class="u-hiddenVisually">Power</span>
-        <span class="ProfileNav-value" data-count="$power"
-          data-is-compact="$is_compact">
-          $power
-          <p class="rate">$rate</p>
-        </span>
-      `.replace(/\$power/g, power)
-       .replace(/\$rate/g, rate(power_value, age))
-       .replace(/\$is_compact/g, is_compact)
-      ;
-      power_node.appendChild(power_link);
-
-      var after_power = stats[stats.length-1].parentNode.parentNode;
-      if (after_power.nextElementSibling) {
-        after_power.parentNode.insertBefore(power_node,
-          after_power.nextElementSibling);
-      } else {
-        after_power.parentNode.appendChild(power_node);
-      }
+      make_stat("power", "Power", "log(Fans)×Fans/Tweet", power);
     }
 
   }
@@ -145,8 +161,7 @@ if (join) {
   GM_addStyle(`
     .ProfileNav-item { vertical-align:top; margin-top:-1.2ex; }
     .ProfileNav-item p.rate { font-size:67%; font-weight:normal; }
-    .ProfileNav-stat--power:hover,
-    .ProfileNav-stat--power:focus { text-decoration:none; }
+    .ProfileNav-stat--link.gm:hover { cursor:pointer; }
   `);
 
 }
