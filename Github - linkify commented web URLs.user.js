16e48daa00ee3c0bb904ccacf6d4a22ca6ffb4eb
Adam Katz <github@khopis.com>
2019-07-20 14:53:11 -0400

    Github linkify should end links at asterisks

--- Github - linkify commented web URLs.user.js
+++ Github - linkify commented web URLs.user.js
@@ -5,12 +5,18 @@
 // @include     https://github.com/*
 // @grant       none
 // @author      Adam Katz
-// @version     1
-// @copyright   2016+ by Adam Katz
-// @license     GPL v3
-// @licstart    The following is the entire license notice for this script.
+// @version     1.0.20190209
+// @license     GPL
+// ==/UserScript==
+
+// WARNING: This "shouldn't" run due to Github's Content Security Policy,
+// but when it updates (from Github?), it no longer violates the policy
+// and it then starts working. I don't fully understand it.
+// See also https://github.com/greasemonkey/greasemonkey/issues/2046
+// and https://bugzilla.mozilla.org/show_bug.cgi?id=1267027
+
 /* 
- * Copyright (C) 2016  Adam Katz
+ * Copyright (C) 2016+ by Adam Katz
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of version 3 of the GNU General Public License as published
@@ -21,14 +27,12 @@
  *
  * Beerware: If you think this is worth it, you are welcome to buy me a beer.
  */ 
-// @licend      The above is the entire license notice for this script.
-// ==/UserScript==
 
 // This ignores .pl-s (strings) because it may be **constructing** a URL
 var plc = document.querySelectorAll(".pl-c");
 for (var c = 0, cl = plc.length; c < cl; c++) {
   plc[c].innerHTML = plc[c].innerHTML
     // avoids ampersands (escaped ampersands are okay) and trailing punctuation
-    .replace(/\b(https?:\/\/(?:[^&\s]+(?:&amp;)*)+[^\s;?.!,<>()\[\]{}'"&])/ig,
+    .replace(/\b(https?:\/\/(?:[^&\s]+(?:&amp;)*)+[^\s;?.!,<>()\[\]{}'"&*])/ig,
              '<a href="$1">$1</a>');
 }
