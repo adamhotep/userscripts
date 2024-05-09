8bd5ec789f44d1e301d7898b77b0acd76cf85353
Adam Katz <6454774+adamhotep@users.noreply.github.com>
2024-05-09 18:40:10 -0400

    colors, dark mode, reset, no auto-capitalization

--- Aspell SCOWL lookup - Mobile UI.user.js
+++ Aspell SCOWL lookup - Mobile UI.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name	Aspell SCOWL English Speller Word Lookup - Mobile UI
+// @name	Aspell SCOWL English Speller Word Lookup - Mobile & Desktop
 // @namespace	https://github.com/adamhotep/userscripts
 // @author	Adam Katz
-// @version	0.1.20230131.6
+// @version	0.1.20240509.0
 // @match	*://app.aspell.net/lookup*
-// @grant	GM_addStyle
+// @require	https://github.com/adamhotep/nofus.js/raw/main/nofus.js
+// @grant	none
 // ==/UserScript==
 
 // Copyright 2023+ by Adam Katz, GPL v3+ {{{
@@ -22,57 +23,67 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>. }}}
 
-if (typeof GM_addStyle == 'undefined') { // {{{
-  function GM_addStyle(aCss) {
-    'use strict';
-    let head = document.head;
-    if (head) {
-      let style = document.createElement('style');
-      style.setAttribute('type', 'text/css');
-      style.textContent = aCss;
-      head.appendChild(style);
-      return style;
-    }
-    return null;
-  }
-} // }}}
+var style = nf.style$(`
 
-// Returns object(s) matched as queried via CSS
-// q$(css)           =>  document.querySelector(css)
-// q$(css, elem)     =>  elem.querySelector(css)
-// q$(css, true)     =>  document.querySelectorAll(css)
-// q$(css, elem, 1)  =>  elem.querySelectorAll(css)
-function q$(css, up = document, all = 0) { // by Adam Katz, github.com/adamhotep
-  if (all === 0 && typeof up != "object") { all = up; up = document; }
-  if (all) { return up.querySelectorAll(css); }
-  else     { return up.querySelector(css); }
-}
+  :root	{ --link: LinkText; }
 
-GM_addStyle(`
-
+  a:hover	{ text-decoration:none; }
   table, textarea { width:100%; }
-  table { border:none; border-spacing:0; }
-  td, th { border:solid 1px #aaaa; }
-  body, th { font-family:sans-serif; }
+  table 	{ border:none; border-spacing:0; }
+  td, th	{ border:solid 1px #aaaa; }
+  body, th	{ font-family:sans-serif; }
   textarea, td:first-child { font-family:monospace; }
+  footnote	{ color:#755; font-size:80%; vertical-align:top; }
   #footnotes:not(.expanded) {
     height:1.15em; overflow-y:hidden; transition:height 1.5s;
   }
-  #footnotes.expanded { background-color:Canvas; color:CanvasText; }
-  #footnotes > b { color:LinkText; cursor:pointer; }
+  #footnotes > b	{ color:var(--link); cursor:pointer; }
   #footnotes          > b::after { content:" +"; }
   #footnotes.expanded > b::after { content:" −"; }
+
+  td[align]:nth-child(6) {
+    color:#000; background-color:#fff;
+    text-shadow:#fff 0 0 1ex, #fff 0 0 2ex;
+  }
 
   /* Eh? I can't just say "mobile" here?
    * max-width doesn't seem to work right, and browsers seem to lie for @media
    */
   @media screen and (orientation:portrait) {
     body, td, th, textarea, input, select { font-size:1.5rem; }
-    textarea { font-size:1.7rem; }
-    button { font-size:2.5rem !important; margin:1vw 0.5vw; }
+    textarea	{ font-size:1.7rem; }
+    button	{ font-size:2.5rem !important; margin:1vw 0.5vw; }
+  }
+
+  @media (prefers-color-scheme: dark) {
+    body, td	{ background-color:#333; color:#fff; }
+    footnote	{ color:#a99; }
+    textarea, input { background-color:#fff3; color:#fff; }
+    font[color="ff0000"], :active { color:#f66; font-weight:bold; }
+    :root	{ --link:#aaf; }
+    :link	{ color:var(--link); }
+    :visited	{ color:#d9f; }
   }
 
 `);
+
+// Color footnotes
+q$(`td`, 1).forEach(td => {
+  td.innerHTML = td.innerHTML.replace(/(\[.\])/g, '<footnote>$1</footnote>');
+});
+
+// Color word frequency by rarity
+q$(`td[align]:nth-child(6)`, 1).forEach(col => {
+  let freq = parseFloat(col.innerText);
+  let cell = col.parentElement.children;
+  if (freq < 0.01 && cell[2].innerText == "") {	// rare
+    let rgb = 128 + 12800 * freq;	// 0-128 out of 255
+    col.style.backgroundColor = `rgb(${rgb}, ${rgb}, 255)`;
+  } else if (freq >= 1) {	// common
+    let rgb = 200 - 2 * freq;	// 0-200 out of 255, inverted
+    col.style.backgroundColor = `rgb(255, ${rgb}, ${rgb})`;
+  }
+});
 
 // Collapse footnotes
 var footnotes = q$('table + p');
@@ -87,4 +98,12 @@ if (footnotes) {
   footnotes_intro.addEventListener("click", function() {
     footnotes.classList.toggle("expanded");
   }, false);
+}
+
+// Actually make the reset button work
+var reset = q$(`form button[type="reset"]`);
+var textarea = q$(`form textarea`);
+if (reset && textarea) {
+  reset.addEventListener("click", function() { textarea.value = ""; });
+  textarea.autocapitalize = "off";	// no mobile browser auto-capitalization
 }
